996ada830b3406944f1642a49e555c40ffc64f2d
Adam Piskorski <adam@piskorski.me>
2020-09-05 13:12:29 +0200

    feat: Add bot DMing and logging

--- pyconza_regbot/bot.py
+++ pyconza_regbot/bot.py
@@ -1,17 +1,37 @@
+import logging
 import os
+import sys
 
 import discord
 
+from roles import ROLES
+
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+
 TOKEN = os.getenv("DISCORD_TOKEN")
 assert TOKEN, "A discord token is expected!"
+
+
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to the following guilds:")
+    logging.info(f"{client.user} has connected to the following guilds:")
     for guild in client.guilds:
-        print(f"{guild.name} ID: {guild.id}")
+        logging.info(f"{guild.name} ID: {guild.id}")
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f"Welcome {member.name} to PyConZA 2020!")
 
 
 client.run(TOKEN)
