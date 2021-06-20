61aaa75aeb3115dde466ccc2b934c97de662afd4
Adam Piskorski <adam@piskorski.me>
2021-06-20 19:21:33 +0200

    feat: Add get_bool_env helper

--- regbot/helpers.py
+++ regbot/helpers.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 import logging
 import os
+from dataclasses import dataclass
+from distutils.util import strtobool
+from typing import Optional
 
-from discord import Guild, TextChannel, Role
+from discord import Guild, Role, TextChannel
 from discord.utils import get
 
 from regbot import bot
@@ -24,6 +26,14 @@ def get_int_env(env_name: str) -> int:
     value = int(os.getenv(env_name, "0"))
     assert value, f"The environmental variable {env_name} was not found!"
     return value
+
+
+def get_bool_env(environ_var: str, default: Optional[bool] = False) -> bool:
+    """Somewhat reliably returns a boolean value based on various kinds of `truthy` or
+    `falsy` string values in the environment.
+    """
+    value = os.getenv(environ_var, str(default))
+    return bool(strtobool(value))
 
 
 LOG_CHANNEL = get_int_env("DISCORD_LOG_CHANNEL_ID")
