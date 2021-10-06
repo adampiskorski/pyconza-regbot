4c27c06973d40a5cbaf9333c1ee9207bf98eb79a
Adam Piskorski <adam@piskorski.me>
2021-10-06 20:57:51 +0200

    fix: Add intent to detect if member is part of the guild

--- regbot/__init__.py
+++ regbot/__init__.py
@@ -2,6 +2,7 @@ __version__ = "0.1.0"
 
 
 from discord.ext import commands
+import discord
 
-
-bot = commands.Bot(command_prefix="!", description="Registration bot")
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", description="Registration bot", intents=intents)
