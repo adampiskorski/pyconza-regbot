f311dc373b3b9eb4af8b183a9c9ac5a2d7d25a6d
Adam Piskorski <adam@piskorski.me>
2021-10-06 20:58:12 +0200

    feat: Protect barcode when mentioned in public

--- regbot/commands.py
+++ regbot/commands.py
@@ -19,20 +19,22 @@ if FEATURE_REGISTRATION:
     @bot.command("register")
     async def register(ctx, barcode: str):
         """Registers the calling user based on their Quicket ticket barcode number."""
+        if ctx.channel.type.name != "private":
+            await ctx.message.delete()
         ticket = await get_ticket_by_barcode(barcode)
         server_info = await ServerInfo.get()
         member = server_info.guild.get_member(ctx.author.id)
         if member is None:
             return await log("None object member encountered in registration call.")
         assistance = (
-            "If you need assistance, then please ask for assistance from the "
+            "\nIf you need assistance, then please ask for assistance from the "
             f"{server_info.help_desk.mention} or a/an {server_info.registration.name}"
         )
 
         if ticket is None:
             await ctx.send(
-                f"Sorry {member.name}, I could not find a ticket with the barcode "
-                f"{barcode}. {assistance}"
+                f"Sorry {member.name}, I could not find a ticket with the given barcode. "
+                f"{assistance}"
             )
             return await log(
                 f"{member.name} tried and failed to register a ticket with the "
@@ -41,7 +43,7 @@ if FEATURE_REGISTRATION:
 
         if await is_ticket_used(ticket):
             await ctx.send(
-                f"Sorry {member.name}, your ticket with barcode {barcode} was already "
+                f"Sorry {member.name}, your ticket with the given barcode was already "
                 f"used! {assistance}"
             )
             return await log(
